e9d09f6db82b569f356398a565b00bafda3b5ed0
sakibalamin <sakibalamin@appscode.com>
2021-02-10 19:15:42 +0600

    var, conditional added

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+# This is just a comment!
+
 # ECHO COMMAND
 echo Hello World!
 
@@ -9,3 +11,32 @@ echo Hello World!
 NAME="Sakib"
 echo "My name is $NAME"
 echo "My name is ${NAME}"
+
+# USER INPUT, -p for prompt()
+read -p "Enter your name: " NAME
+echo "Hello ${NAME}"
+
+# SIMPLE IF STATEMENT, closing if with fi
+if [ "$NAME" == "Sakib" ]
+then
+ echo "Your name is Sakib!"
+fi 
+
+# IF-ELSE
+if [ "$NAME" == "Sakib" ]
+then
+ echo "Your name is Sakib!"
+else
+ echo "Your name is NOT Sakib!"
+fi 
+
+# ELSE-IF (elif)
+if [ "$NAME" == "Sakib" ]
+then
+ echo "Your name is Sakib!"
+elif [ "$NAME" == "Alamin" ]
+then
+ echo "Your name is Alamin"
+else
+ echo "Your name is NOT Sakib or Alamin!"
+fi 
