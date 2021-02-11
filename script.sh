0c06439f1b535e3d2bb11f304c1eaf0d0a5fccdd
sakibalamin <sakibalamin162@gmail.com>
2021-02-11 20:04:53 +0600

    file added

--- script.sh
+++ script.sh
@@ -60,3 +60,23 @@ then
 else
 echo "X is less than Y"
 fi
+
+# FILE CONDITIONS
+# -d file   True if the file is a directory
+# -e file   True if the file exists (note that this is not particularly portable, thus -f is generally used)
+# -f file   True if the provided string is a file
+# -g file   True if the group id is set on a file
+# -r file   True if the file is readable
+# -s file   True if the file has a non-zero size
+# -u    True if the user id is set on a file
+# -w    True if the file is writable
+# -x    True if the file is an executable
+
+FILE="test.txt"
+if [ -f "$FILE" ]
+then
+ echo "$FILE is a file"
+else
+ echo "$FILE is not a file"
+fi
+
